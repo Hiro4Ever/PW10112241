d9e06ced6b89b9f84c30b06dddb391aa7b02485d
Hiro4Ever <90472510+Hiro4Ever@users.noreply.github.com>
2024-05-27 10:51:29 -0700

    ProyectoView, RegistrarCliente

--- proyectoArreglado/frontend/tienda/src/main.js
+++ proyectoArreglado/frontend/tienda/src/main.js
@@ -4,8 +4,28 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB4-53KuTZUN__ffdQi1AK8EnOkzmg5lBQ",
+  authDomain: "tienda-2e5ce.firebaseapp.com",
+  projectId: "tienda-2e5ce",
+  storageBucket: "tienda-2e5ce.appspot.com",
+  messagingSenderId: "616628270902",
+  appId: "1:616628270902:web:38d057b83986a7089ae597",
+  measurementId: "G-T1NNVXT3VG"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
 const app = createApp(App)
 
-app.use(router)
+app.use(router);
 
 app.mount('#app')
